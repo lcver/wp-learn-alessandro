f0878af5df6244d5956a6b3ac0fb77c6d1aa89a9
lcver <crackover1092@gmail.com>
2022-10-24 15:28:52 +0700

    [add] Part 5 create custom and specialized page template

---
+++ app/public/wp-content/themes/awesometheme/page-notitle.php
@@ -0,0 +1,22 @@
+<?php 
+
+/*
+    Template Name: Page no Title
+*/
+
+get_header() ?>
+    
+    <?php if( have_posts() ):
+    
+        while ( have_posts() ) : the_post(); ?>
+        
+            <h1>This is Static Title</h1>
+            <small>Posted on: <?php the_time('F j, Y') ?> at <?php the_time('g:i a') ?>, in <?php the_category(); ?></small>
+            <p><?php the_content(); ?></p>
+
+            <hr/>
+        <?php endwhile;
+
+    endif; ?>
+
+<?php get_footer() ?>
