240e957cdaba6f221742fd85fece556fd27e68f3
lcver <crackover1092@gmail.com>
2022-10-24 18:24:09 +0700

    [add] Part 7 add and create post formats

--- app/public/wp-content/themes/awesometheme/index.php
+++ app/public/wp-content/themes/awesometheme/index.php
@@ -3,13 +3,10 @@
     <?php if( have_posts() ):
     
         while ( have_posts() ) : the_post(); ?>
-        
-            <h3><?php the_title(); ?></h3>
-            <div class="thumbnail-img">><?php the_post_thumbnail('large'); ?></div>
-            <small>Posted on: <?php the_time('F j, Y') ?> at <?php the_time('g:i a') ?>, in <?php the_category(); ?></small>
-            <p><?php the_content(); ?></p>
 
-            <hr/>
+            <!-- Content-[aside, image] -->
+            <?php get_template_part('content', get_post_format()); ?>
+
         <?php endwhile;
 
     endif; ?>
