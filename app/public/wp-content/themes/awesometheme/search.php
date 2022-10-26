151802c6a28c54989473c2846ba73cf9361fc418
lcver <crackover1092@gmail.com>
2022-10-26 19:21:26 +0700

    [add] Part 12 create custom search form and manage search result page

---
+++ app/public/wp-content/themes/awesometheme/search.php
@@ -0,0 +1,24 @@
+<?php get_header() ?>
+
+<div class="row">
+    <div class="col-xs-12 col-sm-8">
+        <div class="row text-center no-margin">
+            <?php if( have_posts() ):
+            
+                while ( have_posts() ) : the_post(); ?>
+
+                    <?php get_template_part('content', 'search') ?>
+
+                <?php endwhile;
+
+            endif; ?>
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-4">
+        <?php get_sidebar() ?>
+    </div>
+
+</div>
+
+<?php get_footer() ?>
---
+++ app/public/wp-content/themes/awesometheme/searchform.php
@@ -0,0 +1,3 @@
+<form action="<?php echo home_url( '/' ) ?>" method="get">
+    <input type="search" name="s" class="form-control" placeholder="Search" title="Search" value="<?php echo get_search_query() ?>" />
+</form>
